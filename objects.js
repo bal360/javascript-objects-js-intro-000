3e5b444c50122e0802584475f94abe9986f9ca31
Blake A Long <blake.a.long@gmail.com>
2019-02-27 20:41:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
+function removeFromPlayList(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
