b3ea0b90bad4550d9854975e69730e7534ad23e0
Blake A Long <blake.a.long@gmail.com>
2019-02-27 20:56:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = { 'SageFrancis': 'MakeShift Patriot',
                         'Sole': 'Bottle of Humans', 
                   'Vinnie Paz': 'Role of life'};
 
-function updatePlaylist(obj, key, value) {
+function updatePlaylist(playlist, 'Ceschi', '') {
   return Object.assign({}, obj, { [key]: value});
 }
